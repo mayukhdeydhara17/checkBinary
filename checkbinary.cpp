d4642621d286bc7bb78715111d1f2f63cd4b5597
Mayukh Dey Dhara <mayukhdeydhara17@gmail.com>
2026-08-17 14:07:31 +0530

    complement added

--- checkbinary.cpp
+++ checkbinary.cpp
@@ -8,6 +8,9 @@ class binary
 public:
     void read();
     void chk_bin();
+    void ones_complement();
+    void display();
+    void display_complement();
 };
 
 void binary ::read()
@@ -27,11 +30,46 @@ void binary ::chk_bin()
         }
     }
 }
+void binary ::ones_complement(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+void binary ::display(void)
+{
+    cout << "DISPLAYING YOUR BINARY NUMBER" << endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout << endl;
+}
+void binary ::display_complement(void)
+{
+    cout << "DISPLAYING YOUR COMPLEMENTED BINARY NUMBER" << endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout << endl;
+}
 
 int main()
 {
     binary b;
     b.read();
     b.chk_bin();
+    b.display();
+    b.ones_complement();
+    b.display_complement();
     return 0;
 }
